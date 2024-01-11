40f2ca572b17c85afa7f0c9cb5f4ed4f0913c03f
bajshorya <jain.shorya11@gmail.com>
2024-01-12 01:30:42 +0530

    advanced fun

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,44 @@
+const todoList=[{
+    name:'',
+    dueDate:''
+}];
+
+renderTodoList();
+
+function renderTodoList(){
+    let todoListHTML=' ';
+
+    todoList.forEach(function(todoObject,index){
+        const name=todoObject.name;
+        const dueDate=todoObject.dueDate;
+        const html=` 
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+            todoList.splice(${index},1);
+            renderTodoList();
+        " class="delete-todo-button"> Delete </button> 
+        `;
+        todoListHTML+=html;
+    });
+   //used for each loop which is more optimal and easy to read and use than the for loop !!!
+
+    document.querySelector('.js-todo-list').innerHTML=todoListHTML;
+}
+function addTodo(){
+    const inputElement=document.querySelector('.js-name-input');
+    const name=inputElement.value;
+
+    const dateInputElement=document.querySelector('.js-due-date-input');
+    const dueDate=dateInputElement.value;
+
+    todoList.push({
+        name:name,
+        dueDate:dueDate
+        //name,
+        //dueDate same as above two lines (shorthand property)
+    });
+    inputElement.value=' ';
+    
+    renderTodoList();
+}
