e2b2e375acaed9ece94ba82f59aaf77115df7b6f
bajshorya <jain.shorya11@gmail.com>
2024-01-18 23:40:27 +0530

    seperate function for priceCents in another jsfile

--- amazon/amazon/scripts/amazon.js
+++ amazon/amazon/scripts/amazon.js
@@ -1,5 +1,6 @@
 import { cart,addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 //imported cart variable from cart.js
 //a module import should always be written on top of the file 
 // to avoid naming conflicts write as _____ after the
@@ -30,7 +31,7 @@ products.forEach((product)=>{
         </div>
 
         <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
+        $${formatCurrency(product.priceCents )}
         </div>
 
         <div class="product-quantity-container">
--- amazon/amazon/scripts/checkout.js
+++ amazon/amazon/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML='';
 
@@ -30,10 +31,10 @@ cart.forEach((cartItem)=>{
             ${matchingProduct.name}
             </div>
             <div class="product-price">
-            $${matchingProduct.priceCents/100}
+            $${formatCurrency(matchingProduct.priceCents)}
             </div>
             <div class="product-quantity">
-            <span>
+            <span >
                 Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">
