d9addd67cb31a64bc92a65ea0297b5ce514de893
bajshorya <jain.shorya11@gmail.com>
2024-01-21 23:54:36 +0530

    checking

--- amazon/amazon/data/products.js
+++ amazon/amazon/data/products.js
@@ -1,10 +1,10 @@
-export function getProduct(productId){
+export function getProduct(productId) {
   let matchingProduct;
-    
+
   products.forEach((product) => {
-      if (product.id === productId) {
-          matchingProduct = product;
-      }
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
   });
 
   return matchingProduct;
--- amazon/amazon/scripts/amazon.js
+++ amazon/amazon/scripts/amazon.js
@@ -1,91 +1,81 @@
-import { cart,addToCart } from "../data/cart.js";
-import { products } from "../data/products.js";
-import { formatCurrency } from "./utils/money.js";
-//imported cart variable from cart.js
-//a module import should always be written on top of the file 
-// to avoid naming conflicts write as _____ after the
-// name of the module ,this will allow you to use the
-// same word in the file again  cart as myCart
-// const cart=[]; then we can use thia
- 
-let productsHTMl='';
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
-products.forEach((product)=>{
-    productsHTMl=productsHTMl+`
-        <div class="product-container">
-        <div class="product-image-container">
+let productsHTML = '';
+
+products.forEach((product) => {
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
         <img class="product-image"
-            src="${product.image}">
-        </div>
+          src="${product.image}">
+      </div>
 
-        <div class="product-name limit-text-to-2-lines">
+      <div class="product-name limit-text-to-2-lines">
         ${product.name}
-        </div>
+      </div>
 
-        <div class="product-rating-container">
+      <div class="product-rating-container">
         <img class="product-rating-stars"
-            src="images/ratings/rating-${product.rating.stars * 10}.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
-            ${product.rating.count}
+          ${product.rating.count}
         </div>
-        </div>
+      </div>
 
-        <div class="product-price">
-        $${formatCurrency(product.priceCents )}
-        </div>
+      <div class="product-price">
+        $${formatCurrency(product.priceCents)}
+      </div>
 
-        <div class="product-quantity-container">
+      <div class="product-quantity-container">
         <select>
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
         </select>
-        </div>
+      </div>
 
-        <div class="product-spacer"></div>
+      <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+      <div class="added-to-cart">
         <img src="images/icons/checkmark.png">
         Added
-        </div>
+      </div>
 
-        <button class="add-to-cart-button button-primary   js-add-to-cart " data-product-id="${product.id}">
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
         Add to Cart
-        </button>
+      </button>
     </div>
-    `;
-
+  `;
 });
 
-document.querySelector('.js-products-grid').innerHTML=productsHTMl;
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function updateCartQuantity( ){
-      
-    let cartQuantity=0;
-    cart.forEach((cartItem)=>{
-         cartQuantity+=cartItem.quantity;
-    });
+function updateCartQuantity() {
+  let cartQuantity = 0;
 
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
 
-    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
-
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 }
 
-document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
-    button.addEventListener('click',()=>{
-       const productId= button.dataset.productId;
-       
-        
-       addToCart(productId);
-       updateCartQuantity();
-    
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
     });
-});
-//data attribute allows us to attach any info to an element ,has to start with "data-",then any name can be given 
+  });
