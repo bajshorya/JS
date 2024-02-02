708322ed62ed075c4f6e14da1f80ff0d07222330
bajshorya <jain.shorya11@gmail.com>
2024-02-03 00:19:07 +0530

    commented

--- notes.js
+++ notes.js
@@ -89,17 +89,17 @@
 // await pauses the execution of its surrounding async function
 //until the promise is settled
 
-function api() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('weather data');
-            resolve(200);
-        }, 2000);
-    });
-}
+// function api() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('weather data');
+//             resolve(200);
+//         }, 2000);
+//     });
+// }
 
 
-async function getWeatherData(){
-    await api();
-    await api();
-}
+// async function getWeatherData(){
+//     await api();
+//     await api();
+// }
